a50d69309096fa6a9093b0b6f0b0f3a2d0d4550a
惧 <62569515+YouMostFear@users.noreply.github.com>
2023-03-18 15:35:04 +0800

    Update drpy.js

--- txt/pluto/drpy.js
+++ txt/pluto/drpy.js
@@ -1,7 +1,7 @@
 import ch from './cheerio.min.js';
 // import 'http://192.168.10.99:5705/txt/pluto/drT.js';
 // import 'http://192.168.3.239:5705/txt/pluto/drT.js';
-import 'http://gitcode.net/qq_32394351/dr_py/-/raw/master/txt/pluto/drT.js';
+import 'https://raw.githubusercontent.com/YouMostFear/op/master/txt/pluto/drT.js';
 // import 模板 from 'https://gitcode.net/qq_32394351/dr_py/-/raw/master/js/模板.js'
 // var rule = Object.assign(模板.首图2,{
 // host: 'https://www.zbkk.net',
